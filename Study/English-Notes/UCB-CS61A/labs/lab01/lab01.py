3ad84e3898223bf0273d79d228057eccf980c877
Chris White <chriswhite33333@gmail.com>
2023-05-23 08:03:10 +0800

    vault backup: 2023-05-23 08:03:10

---
+++ Study/English-Notes/UCB-CS61A/labs/lab01/lab01.py
@@ -0,0 +1,65 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    total, cal = 1, 0
+    while cal < k:
+        total, cal = total * (n - cal), cal + 1
+    return total
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    sum, digit = 0, y
+    while digit > 0:
+        sum, digit = sum + digit % 10, digit // 10
+    return sum
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    prev, digit = 0, n
+    while digit > 0:
+        if digit % 10 == 8:
+            if prev:
+                return True
+            else:
+                prev = 1
+        else:
+            prev = 0
+        digit //= 10
+    return False
+
+
---
+++ Study/English-Notes/UCB-CS61A/myProgress.md
@@ -0,0 +1,17 @@
+## Homework
+
+* [x] Hw01
+* [x] Hw02
+
+​	Q4 left
+
+* [ ] Hw03
+
+## Labs
+
+- [x] Lab00
+- [x] Lab01
+- [ ] Lab02
+
+
+
